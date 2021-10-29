3325f29446be1acf74075488a33e47023f43359a
Shivansh Saini <shivanshs9@gmail.com>
2021-10-30 03:18:26 +0530

    fix: set current context as variable

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,11 +1,12 @@
 import * as core from '@actions/core'
 import { info, error } from '@actions/core'
 import { ICluster } from "./clusters"
-import * as exec from 'execa'
+import exec from 'execa'
 
 export async function deployService(cluster: ICluster) {
     core.startGroup('Deploy Service')
-    let cmd = `garden deploy --env ${cluster.gardenEnv}`
+    const { stdout: context } = await exec('kubectl', ['config', 'current-context'])
+    let cmd = `garden deploy --env ${cluster.gardenEnv} --var kubeContext=${context}`
     let cmdEnv = { GARDEN_LOGGER_TYPE: "basic", NAMESPACE: "cd" }
     info(`Executing "${cmd}" with env: ${JSON.stringify(cmdEnv)}`)
     try {
